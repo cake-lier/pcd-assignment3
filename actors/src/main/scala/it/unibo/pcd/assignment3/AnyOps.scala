3ccdc2208d78b5fd63b16b9bd8f137f8a167bd3c
Matteo Castellucci <matteino98@gmail.com>
2021-11-29 23:11:51 +0100

    Add code for wartremover compliance

---
+++ actors/src/main/scala/it/unibo/pcd/assignment3/AnyOps.scala
@@ -0,0 +1,15 @@
+package it.unibo.pcd.assignment3
+
+object AnyOps {
+
+  @SuppressWarnings(Array("org.wartremover.warts.Equals"))
+  implicit final class AnyOps[A](self: A) {
+    def ===(other: A): Boolean = self == other
+  }
+
+  @specialized
+  def discard(evaluateForSideEffectOnly: Any): Unit = {
+    val _ = evaluateForSideEffectOnly
+    ()
+  }
+}
